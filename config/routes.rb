34c53a2fa4c66262cc794bb71101b8a7427485a7
bshap27 <rshapiro3@gmail.com>
2017-09-12 22:53:05 -0400

    add user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :users
+
   resources :pets
   root 'pets#index'
   # The priority is based upon order of creation: first created -> highest priority.
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170912195909) do
+ActiveRecord::Schema.define(version: 20170913023736) do
 
   create_table "pets", force: :cascade do |t|
     t.datetime "sitecreated"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20170912195909) do
     t.string   "breed"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
