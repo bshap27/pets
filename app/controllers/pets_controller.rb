572bb9d37c9fce0e7b61a3a49d939e1e57ee9181
bshap27 <rshapiro3@gmail.com>
2017-09-13 19:29:29 -0400

    working state

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,6 +1,6 @@
 class PetsController < ApplicationController
 
-  before_action :set_pet, only: [:show, :edit, :update, :destroy]
+  # before_action :set_pet, only: [:show, :edit, :update, :destroy]
   
   def index
     @pets = Pet.all
