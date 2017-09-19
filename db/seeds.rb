7d51b9c7d70df973cb34a7914c85f6d22483ad2d
bshap27 <rshapiro3@gmail.com>
2017-09-19 15:36:02 -0400

    crushed seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,49 +1,24 @@
-# Pet.create([
-#   {petfinderid: '31604370', name: 'Leggs', animal: 'Dog', mix: 'yes', sex: 'F'},
-#   {petfinderid: '37848260', name: 'Wednesday', animal: 'Dog', mix: 'yes', sex: 'F'},
-#   {petfinderid: '38067335', name: 'Sandy', animal: 'Dog', mix: 'yes', sex: 'F'},
-#   {petfinderid: '33701931', name: 'Gucci', animal: 'Dog', mix: 'yes', sex: 'F'},
-#   {petfinderid: '35019990', name: 'Roxy', animal: 'Dog', mix: 'yes', sex: 'F'},
-#   {petfinderid: '38781233', name: 'Alex', animal: 'Dog', mix: 'yes', sex: 'M'},
-#   {petfinderid: '38781283', name: 'Alex', animal: 'Dog', mix: 'yes', sex: 'M'},
-#   {petfinderid: '37405452', name: 'Holiday', animal: 'Dog', mix: 'no', sex: 'F'},
-#   {petfinderid: '36449269', name: 'Hope', animal: 'Dog', mix: 'yes', sex: 'F'},
-#   {petfinderid: '36974356', name: 'Calvin', animal: 'Dog', mix: 'yes', sex: 'M'},
-#   {petfinderid: '38788046', name: 'Lucky', animal: 'Dog', mix: 'yes', sex: 'M'},
-#   {petfinderid: '38799307', name: 'Cupid', animal: 'Dog', mix: 'yes', sex: 'M'},
-#   {petfinderid: '38588753', name: 'Harley', animal: 'Dog', mix: 'yes', sex: 'F'},
-#   {petfinderid: '39245659', name: 'Anna Sui', animal: 'Dog', mix: 'yes', sex: 'F'},
-#   {petfinderid: '39248483', name: 'Star', animal: 'Dog', mix: 'yes', sex: 'F'},
-#   {petfinderid: '39313093', name: 'Ronin', animal: 'Dog', mix: 'no', sex: 'M'},
-#   {petfinderid: '39360015', name: 'Riley', animal: 'Dog', mix: 'yes', sex: 'M'},
-#   {petfinderid: '39359984', name: 'Rex', animal: 'Dog', mix: 'yes', sex: 'M'},
-#   {petfinderid: '39359139', name: 'Oliver', animal: 'Dog', mix: 'yes', sex: 'M'},
-#   {petfinderid: '38244882', name: 'Rocky', animal: 'Dog', mix: 'no', sex: 'M'},
-#   {petfinderid: '37253070', name: 'Hero', animal: 'Dog', mix: 'yes', sex: 'M'},
-#   {petfinderid: '39368239', name: 'William', animal: 'Dog', mix: 'yes', sex: 'M'},
-#   {petfinderid: '36496131', name: 'WEBSTER', animal: 'Dog', mix: 'no', sex: 'M'}
-# ])
-
-# Breed.create([
-#   {name: 'Havanese'},
-#   {name: 'Pug'},
-#   {name: 'Cocker Spaniel'},
-#   {name: 'Spaniel'}
-# ])
-
 ### API limits:
 # Total requests per day: 10,000
 # Records per request: 1,000
 # Maximum records per search: 2,000
 
-count = 10
-reps = 3
+count = 100
+reps = 50
 counter = 0
 offset = 0
 p = count
+sizes = ["S", "M", "L"]
+round = 2
 
-while p == count && counter <= reps
-  p = PetfinderApi.find_new_pets('dog', 10014, :offset => offset, :count => count)
+while p == count && counter < reps && round < sizes.length
+  p = PetfinderApi.find_new_pets('dog', 10014, :offset => offset, :count => count, :size => sizes[round])
   offset += count
   counter += 1
+  if (p < count || offset >= 2000) && round < sizes.length
+    round += 1
+    offset = 0
+    counter = 0
+  end
+  puts "Size #{sizes[round]} Round #{counter}: #{count} results."
 end
