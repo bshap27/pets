aace02e0d9cc320708d8aef23fe0f05b7a2b7901
bshap27 <rshapiro3@gmail.com>
2017-09-22 19:38:21 -0400

    dont run hub_exclude every time server starts

--- app/models/concerns/hub_exclude.rb
+++ app/models/concerns/hub_exclude.rb
@@ -11,6 +11,6 @@ end
 
 sophie_breeds = ["Afghan Hound", "Akita", "Alabai", "Alaskan Malamute", "American Bandogge", "Australian Cattle Dog / Blue Heeler", "Basenji", "Basset Hound", "Bedlington Terrier", "Bernese Mountain Dog", "Bloodhound", "Boerboel", "Boxer", "Bullmastiff", "Mastiff", "Bully Kutta", "American Bulldog", "English Bulldog", "Cane Corso Italiano", "Caucasian Ovcharka", "Chow Chow", "Dalmatian", "Doberman Pinscher", "Dogo Argentino", "Fila Brasilerio", "German Shepherd Dog", "German Shepherd", "Shepherd", "Great Dane", "Gull Dong", "Kangal", "Keeshond Mastiff", "Keeshond" "Pointer", "Pit Bull Terrier", "Pit Bull", "American Staffordshire Terrier", "Irish Staffordshire Terrier", "Staffordshire Bull Terrier", "Bull Terrier", "Presa Canario", "Rhodesian Ridgeback", "Rottweiler", "Saluki", "Saint Bernard", "Saint Bernard / St. Bernard", "Siberian Husky", "Tibetan Mastiffe", "Tosa Inu", "Weimaraner"]
 
-sophie_breeds.each do |b|
-  update_breed(b)
-end
+# sophie_breeds.each do |b|
+#   update_breed(b)
+# end
