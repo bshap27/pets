720a45b042e2766124f5e8c43f3bc4730fe373f6
bshap27 <rshapiro3@gmail.com>
2017-10-01 19:32:05 -0400

    add status to pets

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170926204218) do
+ActiveRecord::Schema.define(version: 20171001231206) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20170926204218) do
     t.boolean  "new_pet"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.string   "status"
   end
 
   create_table "user_breeds", force: :cascade do |t|
