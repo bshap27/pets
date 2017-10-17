cf0c0a6e7e5f11c9990f770a7a9a67d1360dc659
bshap27 <rshapiro3@gmail.com>
2017-10-17 10:22:29 -0400

    capistrano for rbenv not rvm

--- config/deploy.rb
+++ config/deploy.rb
@@ -34,3 +34,9 @@ set :repo_url, "git@github.com:bshap27/pets.git"
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
+
+set :rbenv_type, :user # or :system, depends on your rbenv setup
+set :rbenv_ruby, '2.2.0'
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+set :rbenv_roles, :all # default value
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,25 @@
+set :port, 22
+set :user, 'becca'
+set :deploy_via, :remote_cache
+set :use_sudo, false
+
+server '45.55.169.248',
+  roles: [:web, :app, :db],
+  port: fetch(:port),
+  user: fetch(:user),
+  primary: true
+
+set :deploy_to, "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
+
+set :ssh_options, {
+  forward_agent: true,
+  auth_methods: %w(publickey),
+  user: 'becca',
+}
+
+set :rails_env, :production
+set :conditionally_migrate, true 
+
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
