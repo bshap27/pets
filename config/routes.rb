db2ad6a894b33499bbb8b7ee4e753213d9ab8037
bshap27 <rshapiro3@gmail.com>
2017-11-22 12:56:33 -0500

    Revert "Merge pull request #11 from bshap27/droplet-mina"
    This reverts commit 834967b79bdd7c5adff5f83f86bb4c78f599396d, reversing
    changes made to 6f80a8b54d4ef9dc9ced53db6d60283d7e2d9d4f.

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   get 'more_pets/:page' => 'pets#more_pets', :as => 'more_pets'
   # get 'page/:id' => 'pets#next_page', :as => 'next_page'
 
-  root 'breeds#index'
+  root 'pets#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
